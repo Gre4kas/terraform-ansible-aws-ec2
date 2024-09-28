2be1a0402b0f12ee0e3fa8f0cb7e1e2a66329a65
Gre4kas <misaa3818@gmail.com>
2024-09-28 20:59:53 +0300

    Terraform fmt

--- terraform/global/s3-beckend/provider.tf
+++ terraform/global/s3-beckend/provider.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 terraform {
-    required_version = ">= 1.0.0"
-    # backend "s3" {
-    #     # Replace this with your bucket name!
-    #     bucket = "terraform-remote-state-bucket-gre4ka"
-    #     key = "global/s3/terraform.tfstate"
-    #     region= "us-east-1"
-    #     # Replace this with your DynamoDB table name!
-    #     dynamodb_table = "terraform-remote-state-dynamo"
-    #     encrypt        = true
-    # } 
+  required_version = ">= 1.0.0"
+#   backend "s3" {
+#     # Replace this with your bucket name!
+#     bucket = "terraform-remote-state-bucket-gre4ka"
+#     key    = "global/s3/terraform.tfstate"
+#     region = "us-east-1"
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "terraform-remote-state-dynamo"
+#     encrypt        = true
+#   }
 }
