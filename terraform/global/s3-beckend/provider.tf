8c8cdb8d86403e0fb196d72245bdb5144a06ada3
Gre4kas <misaa3818@gmail.com>
2024-09-30 11:16:41 +0300

    Delete commentaries

--- terraform/global/s3-beckend/provider.tf
+++ terraform/global/s3-beckend/provider.tf
@@ -4,13 +4,13 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.0.0"
-#   backend "s3" {
-#     # Replace this with your bucket name!
-#     bucket = "terraform-remote-state-bucket-gre4ka"
-#     key    = "global/s3/terraform.tfstate"
-#     region = "us-east-1"
-#     # Replace this with your DynamoDB table name!
-#     dynamodb_table = "terraform-remote-state-dynamo"
-#     encrypt        = true
-#   }
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-remote-state-bucket-gre4ka"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-remote-state-dynamo"
+    encrypt        = true
+  }
 }
